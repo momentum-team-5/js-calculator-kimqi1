f6f4822ca8a9aeff20e36ee0942babbb903ee83e
Kim Leibowitz <kim@brightlightwellness.com>
2020-09-20 22:06:31 -0400

    at least it looks good:)

---
+++ main.js
@@ -0,0 +1,11 @@
+let button = document.querySelector('.flex-container')
+button.addEventListener('click', function(event) )
+
+let inputArray = []
+
+if (target === "=")
+return (eval(input))
+
+function updateDisplay() {
+    const disp
+}
